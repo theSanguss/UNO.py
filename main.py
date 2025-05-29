ea5f57730e0037c5de00b54f115c9f21322df2f1
theSanguss <205304252+theSanguss@users.noreply.github.com>
2025-05-29 19:28:49 +0530

    Test Commit 01

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from sys import exit as sys_exit
 if __name__ == "__main__":
     game = Game()
     game.start()
-    # Ensures safe and clean exit, unsure of its necessity tho
+    # Ensures safe and clean exit, good to have here
     sys_exit(0)
